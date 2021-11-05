38e691ef5f90e3e402b2175d5d3df40fb1537784
patrick charles <pchuck@users.noreply.github.com>
2021-11-05 13:31:17 -0600

    added some predefined scales for quantization

--- krell-clocked.lua
+++ krell-clocked.lua
@@ -24,7 +24,18 @@ V_HYST = 0.1 -- hysteresis voltage
 TRIG = 'rising' -- trigger condition
 
 -- quantization
-local SCALE = {0, 2, 3, 5, 7, 9, 10} -- allowed note values
+-- scales (via bowery/quantizer)
+scales = { ['none']   = { },
+           ['octave'] = {0},
+           ['major' ] = {0, 2, 4, 5, 7, 9, 11},
+           ['harMin'] = {0, 2, 3, 5, 7, 8, 10},
+           ['dorian'] = {0, 2, 3, 5, 7, 9, 10},
+           ['majTri'] = {0, 4, 7},
+           ['dom7th'] = {0, 4, 7, 10},
+           ['wholet'] = {0, 2, 4, 6, 8, 10},
+           ['chroma'] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },}
+-- local SCALE = scales['chroma'] -- for a true 'krell' experience
+local SCALE = scales['dom7th'] -- or, something more melodic
 local TET12 = 12  -- temperament
 local V_PO  = 1.0 -- volts per octave
 
