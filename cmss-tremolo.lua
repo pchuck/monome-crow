8d5f57a402eab71b9be07f152ed4f8ad12ba66e1
patrick charles <pchuck@users.noreply.github.com>
2021-11-09 21:14:31 -0700

    renamed back to tremolo

--- cmss-tremolo.lua
+++ cmss-tremolo.lua
@@ -1,16 +1,16 @@
---- cmss-vibrato - vibrato prototype/collaboration
---    vibrato as in amplitude modulation, or 'tremolo' as in guitar tremolo
+--- cmss-tremolo - tremolo prototype/collaboration
+--    tremolo as in amplitude modulation, not vibrato (as in pitch)
 --    pb. the colorado modular society, sine mountain and patchogram
 --    https://github.com/pchuck/monome-crow
 --
--- A configurable parametric vibrato effect/envelopes generator
+-- A configurable parametric tremolo effect/envelopes generator
 --
 --  in1: gate
 --  in2: v/o pitch
--- out1: vibrato oscillation/decay/envelope 1
--- out2: vibrato oscillation/decay/envelope 2
--- out3: vibrato oscillation/decay/envelope 3
--- out4: vibrato oscillation/decay/envelope 4
+-- out1: tremolo oscillation/decay/envelope 1
+-- out2: tremolo oscillation/decay/envelope 2
+-- out3: tremolo oscillation/decay/envelope 3
+-- out4: tremolo oscillation/decay/envelope 4
 --
 
 -- constants
@@ -20,12 +20,12 @@ V_HYST = 0.1 -- hysteresis voltage
 TRIG = 'rising' -- trigger condition
 GATE = 1 ; PITCH = 2 -- inputs - logical ids of the inputs
 OUTS = { 1, 2, 3, 4 } -- outputs - logical ids of the lfo outputs
--- vibrato envelope default parameters
+-- tremolo envelope default parameters
 V_MIN = 1.0 -- min output voltage
 V_MAX = 8.0 -- max output voltage
-DIV = 50 -- oscillation rate (number of vibrato divisions per tap/clock period)
-TIME_ACCEL = 1.1 -- time compression per vibrato interval
-LEVEL_DECAY = -0.8 -- voltage decay per vibrato interval
+DIV = 50 -- oscillation rate (number of tremolo divisions per tap/clock period)
+TIME_ACCEL = 1.1 -- time compression per tremolo interval
+LEVEL_DECAY = -0.8 -- voltage decay per tremolo interval
 
 -- public/configurable parameters
 public.add('shape', 'sine', -- envelope shape
@@ -52,13 +52,13 @@ function lfo2(sid, pitch, period, offset, level)
    if sid % 2 == 0 then return loop { up, dn } 
    else                 return loop { dn, up } end
    
-   -- pitch currently not used; could affect vibrato rate/depth
-   -- attack/release from vibrato() not used; could layer on an encaps env
+   -- pitch currently not used; could affect tremolo rate/depth
+   -- attack/release from tremolo() not used; could layer on an encaps env
    --   ar(attack, release, public.gain, public.shape)})
 end
 
--- capture pitch, create and trigger the vibrato envelope for specified out
-function vibrato(sid, period, attack, release)
+-- capture pitch, create and trigger the tremolo envelope for specified out
+function tremolo(sid, period, attack, release)
    local pitch = input[PITCH].volts
    for _, v in pairs(OUTS) do
       output[v].action = { lfo2(v, pitch, period, V_MIN, V_MAX) }
@@ -66,7 +66,7 @@ function vibrato(sid, period, attack, release)
    end
 end
 
--- update the vibrato/attack/release parameters and trigger the envelope
+-- update the tremolo/attack/release parameters and trigger the envelope
 time_last = { 0, 0 } -- last time each output's envelope was triggered
 function change(sid)
    local time_now = time()
@@ -75,5 +75,5 @@ function change(sid)
    local attack = frequency / 5
    local release = frequency / 3
    time_last[sid] = time_now -- record the current time
-   vibrato(sid, time_delta, attack, release)
+   tremolo(sid, time_delta, attack, release)
 end
