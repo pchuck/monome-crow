fce659b0b2752bde40fdb0a611a72fae80867b3d
patrick charles <pchuck@users.noreply.github.com>
2021-12-08 09:14:17 -0700

    replaced parameter mapper with tables. added notes regarding lua/crow public tables ranges not working

--- melodicer.lua
+++ melodicer.lua
@@ -15,24 +15,24 @@
 --
 -- Example use case (and associated configuration)...
 --
---   (1) is a lead voice that has 30% probability of triggering notes
+--   (1) is a primary voice that has 40% probability of triggering notes
 --   with a tonal range of 2 octaves. Output 1 has a sequence length of 8
 --   and repeats the pattern 4 times before randomizing.
 --   (1) is clocked by in1 and outputs its gate on out1 and notes on out2.
 --
---   (2) is an auxiliary voice (bass line) that has a 60% chance of
+--   (2) is an auxiliary voice that has a 60% chance of
 --   triggering notes with a tonal range of 1 octave. The sequence length is
 --   2 and patterns are repeated 8 times before randomizing.
 --   (2) is clocked by in2 and outputs its gate on out3 and notes on out4.
 --
---   After starting the script, optionally customize and execute the following:
+--   After starting the script, optionally customize (on the druid console)..
 --
---     public.probability_1=0.7
+--     public.probability_1=0.4
 --     public.level_1=2.0
 --     public.length_1=8
 --     public.repeat_1=4
 --
---     public.probability_2=1.0
+--     public.probability_2=0.6
 --     public.level_2=1.0
 --     public.length_2=2
 --     public.repeat_2=8
@@ -47,20 +47,27 @@ VPO = 1.0 -- volts per octave
 INS =   { 1, 2 } -- logical ids of the inputs
 GATES = { 1, 3 } -- logical ids of the gate outputs
 VOCTS = { 2, 4 } -- logical ids of the note outputs
-MAX_REPEAT = 8 -- maximum value for sequence repeat
+MAX_REPEAT = 8 -- maximum value for sequence repetition
 MAX_LEN = 32 -- maximum value for sequence length
-MAX_V = 10
+MAX_V = 5 -- maximum output voltage
+MAX_V_OFFSET = 5 -- maximum voltage offset
+MAX_PROB = 1.0 -- maximum probability
+REPEAT_RANGE = { 1, MAX_REPEAT } -- range of valid repetitions
+LEN_RANGE = { 1, MAX_LEN } -- range of valid sequence lengths
+V_RANGE = { -MAX_V, MAX_V } -- range of output voltages
+V_OFFSET_RANGE = { -MAX_V_OFFSET, MAX_V_OFFSET } -- range of voltage offsets
+PROB_RANGE = { 0, MAX_PROB } -- range of probabilities
 
 -- global parameters for output voices 1/2
 gate_sequence = { [1] = {}, [2] = {} } -- gate sequence
 note_sequence = { [1] = {}, [2] = {} } -- note sequence
-probabilities = { [1] =  0, [2] =  0 } -- gate probability
-levels        = { [1] =  0, [2] =  0 } -- max output voltage
-offsets       = { [1] =  0, [2] =  0 } -- voltage offset
-lengths       = { [1] =  0, [2] =  0 } -- max sequence length
-repeats       = { [1] =  0, [2] =  0 } -- max sequence repeat
-seq_position  = { [1] =  0, [2] =  0 } -- current sequence position
-rep_position  = { [1] =  0, [2] =  0 } -- current repeat position
+probabilities = { } -- gate probability
+levels        = { } -- max output voltage
+offsets       = { } -- voltage offset
+lengths       = { } -- max sequence length
+repeats       = { } -- max sequence repeat
+seq_position  = { 0, 0 } -- current sequence position
+rep_position  = { 0, 0 } -- current repeat position
                
 -- scales (via bowery/quantizer)
 scale_names = { 'none', 'octave', 'major', 'harMin', 'dorian', 'majTri',
@@ -79,47 +86,31 @@ scale_notes = { ['none']   = 'none',
 --   > public.scale = 'chroma'
 --
 -- probability of triggering an output gate
-public.add('probability_1', 0.3, { 0.0, 1.0 })
-public.add('probability_2', 0.6, { 0.0, 1.0 })
+public.add('probabilities', { 0.4, 0.6 }) -- , PROB_RANGE)
 -- output voltage ranges (v)
-public.add('level_1', 2.0, { 0, MAX_V })
-public.add('level_2', 1.0, { 0, MAX_V })
+public.add('levels', { 2.0, 1.0 }) -- , V_RANGE)
 -- output voltage offsets (v)
-V_RANGE = { -MAX_V / 2, MAX_V / 2 }
-public.add('offset_1', 0.0, V_RANGE)
-public.add('offset_2', 0.0, V_RANGE)
+public.add('offsets', { 0.0, 0.0 }) -- , V_OFFSET_RANGE)
 -- sequence lengths
-public.add('length_1', 8, { 1, MAX_LEN })
-public.add('length_2', 2, { 1, MAX_LEN })
+public.add('lengths', { 8, 2 }) -- , LEN_RANGE)
 -- number of times to repeat sequence before randomizing
-public.add('repeat_1', 4, { 1, MAX_REPEAT })
-public.add('repeat_2', 8, { 1, MAX_REPEAT })
+public.add('repeats', { 4, 8 }) -- , REPEAT_RANGE)
 -- quantization
 public.add('scale', 'chroma', scale_names, -- scale
            function() set_scale(public.scale) end)
-
-
--- map input values to voice table
--- since 'public' doesn't handle tables(?) directly
-function update_values()
-   probabilities[1] = public.probability_1
-   probabilities[2] = public.probability_2
-    levels[1] = public.level_1
-    levels[2] = public.level_2
-   offsets[1] = public.offset_1
-   offsets[2] = public.offset_2
-   repeats[1] = public.repeat_1
-   repeats[2] = public.repeat_2
-   lengths[1] = public.length_1
-   lengths[2] = public.length_2
-end
-   
+--
+-- note: ranges not working w tables? otherwise would clamp w/ something like:
+--   public.add('probabilities', { 0.4, 0.6 }, { PROB_RANGE, PROB_RANGE } )
+--
+public.add('test', 0.5, { 0.0, 1.0 } )
+public.add('test_t', { 0.5, 0.6 }, { 0.0, 1.0 } )
 
 -- generate new random gates and notes (eg. melodicer 'dice' function)
 function randomize(sid)
    for i = 1, MAX_LEN + 1 do
-      note_sequence[sid][i] = math.random() * levels[sid] + offsets[sid]
-      gate_sequence[sid][i] = probabilities[sid] >= math.random()
+      note_sequence[sid][i] = math.random() *
+         public.levels[sid] + public.offsets[sid]
+      gate_sequence[sid][i] = public.probabilities[sid] >= math.random()
    end
 end
 
@@ -127,7 +118,7 @@ end
 function init()
    set_scale(public.scale) -- set quantization
    for _,i in pairs(INS) do
-      update_values()
+--      update_values()
       randomize(i)
       input[i].mode('change', V_THRESH, V_HYST, TRIG) -- trig on clock edge
       input[i].change = function() change(i) end -- trig call-back function
@@ -137,16 +128,17 @@ end
 -- event-handler for clock events
 function change(sid)
 --   print("sid, r, s", sid, rep_position[sid], seq_position[sid])      
-   update_values()
+--   update_values()
    seq_position[sid] = seq_position[sid] + 1 --- increment the sequence position
-   if seq_position[sid] > lengths[sid] then -- if the sequence exceeds length
+   if seq_position[sid] > public.lengths[sid] then -- sequence exceeds length
       seq_position[sid] = 1 -- wrap 
       rep_position[sid] = rep_position[sid] + 1 -- increment the repeat counter
-      if rep_position[sid] > repeats[sid] then -- if repeat max is exceeded
+      if rep_position[sid] > public.repeats[sid] then -- repeats exceeds max
          rep_position[sid] = 1 -- wrap
          randomize(sid) -- randomize the sequence and gates
       end
    end
+   -- if gate, update tone output and trigger
    if gate_sequence[sid][seq_position[sid]] then
       output[VOCTS[sid]].volts = note_sequence[sid][seq_position[sid]]
       output[GATES[sid]](pulse())
