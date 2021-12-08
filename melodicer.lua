fbb66a1a82b71356a72f5193c7be96ba2fcd6e8e
patrick charles <pchuck@users.noreply.github.com>
2021-12-07 20:51:53 -0700

    changed defaults and use case example

--- melodicer.lua
+++ melodicer.lua
@@ -15,12 +15,12 @@
 --
 -- Example use case (and associated configuration)...
 --
---   (1) is a lead voice that has 70% probability of triggering notes
---   with a tonal range of 3 octaves. Output 1 has a sequence length of 8
+--   (1) is a lead voice that has 30% probability of triggering notes
+--   with a tonal range of 2 octaves. Output 1 has a sequence length of 8
 --   and repeats the pattern 4 times before randomizing.
 --   (1) is clocked by in1 and outputs its gate on out1 and notes on out2.
 --
---   (2) is an auxiliary voice (bass line) that has a 100% chance of
+--   (2) is an auxiliary voice (bass line) that has a 60% chance of
 --   triggering notes with a tonal range of 1 octave. The sequence length is
 --   2 and patterns are repeated 8 times before randomizing.
 --   (2) is clocked by in2 and outputs its gate on out3 and notes on out4.
@@ -77,10 +77,10 @@ scale_notes = { ['none']   = 'none',
 --   > public.scale = 'chroma'
 --
 -- probability of triggering an output gate
-public.add('probability_1', 0.7, { 0.0, 1.0 })
-public.add('probability_2', 1.0, { 0.0, 1.0 })
+public.add('probability_1', 0.3, { 0.0, 1.0 })
+public.add('probability_2', 0.6, { 0.0, 1.0 })
 -- output voltage ranges (v)
-public.add('level_1', 3.0, { 0, MAX_V })
+public.add('level_1', 2.0, { 0, MAX_V })
 public.add('level_2', 1.0, { 0, MAX_V })
  -- output voltage offsets (v)
 public.add('offset_1', 0.0, { -MAX_V / 2, MAX_V / 2 })
@@ -133,7 +133,7 @@ end
 
 -- event-handler for clock events
 function change(sid)
-   -- print("sid, r, s", sid, rep_position[sid], seq_position[sid])      
+--   print("sid, r, s", sid, rep_position[sid], seq_position[sid])      
    update_values()
    seq_position[sid] = seq_position[sid] + 1 --- increment the sequence position
    if seq_position[sid] > lengths[sid] then -- if the sequence exceeds length
@@ -145,7 +145,6 @@ function change(sid)
       end
    end
    if gate_sequence[sid][seq_position[sid]] then
-
       output[VOCTS[sid]].volts = note_sequence[sid][seq_position[sid]]
       output[GATES[sid]](pulse())
    end
