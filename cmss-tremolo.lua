aa41df4114260b41d198eeb73a4d86ccd7a05830
patrick charles <pchuck@users.noreply.github.com>
2021-12-10 15:14:21 -0700

    removed unused sid

--- cmss-tremolo.lua
+++ cmss-tremolo.lua
@@ -1,16 +1,16 @@
 --- cmss-tremolo - tremolo prototype/collaboration
---    tremolo as in amplitude modulation, not vibrato (as in pitch)
+--    tremolo as in amplitude modulation, not vibrato (pitch modulation)
 --    pb. the colorado modular society, sine mountain and patchogram
 --    https://github.com/pchuck/monome-crow
 --
--- A configurable parametric tremolo effect/envelopes generator
+-- A configurable parametric tremolo effect/envelope generator
 --
 --  in1: gate
 --  in2: v/o pitch
--- out1: tremolo oscillation/decay/envelope 1
--- out2: tremolo oscillation/decay/envelope 2 (phase shifted)
--- out3: tremolo oscillation/decay/envelope 3
--- out4: tremolo oscillation/decay/envelope 4 (phase shifted)
+-- out1: tremolo oscillation/decay envelope 1
+-- out2: tremolo oscillation/decay envelope 2 (phase shifted)
+-- out3: tremolo oscillation/decay envelope 3
+-- out4: tremolo oscillation/decay envelope 4 (phase shifted)
 --
 
 -- constants
@@ -33,11 +33,11 @@ public.add('shape', 'sin', -- envelope shape
 -- initialization - setup the callbacks for reacting to triggers/gates
 function init()
    input[GATE].mode('change', V_THRESH, V_HYST, TRIG) -- trig on clock edge
-   input[GATE].change = function() change(1) end -- the call-back function
+   input[GATE].change = function() change() end -- the call-back function
 end
 
 -- tremolo lfo w/ support for +/- offsets, shapes and dynamic values
-function tremolo(sid, pitch, period, offset, level)
+function tremolo(oid, pitch, period, offset, level)
    local shape = public.shape -- envelope shape
    local sub = period / public.rate -- time per oscillation, compressed by accel
    local accel = public.accel -- time-based modulation accel/deceleration 
@@ -45,16 +45,16 @@ function tremolo(sid, pitch, period, offset, level)
    local up = to( offset,                     dyn{time=sub}:mul(accel), shape )
    local dn = to( dyn{height=level}:step(ld), dyn{time=sub}:mul(accel), shape )
    -- invert odd/even output waveforms for a leslie speaker-like stereo effect
-   if sid % 2 == 0 then return loop { up, dn } 
+   if oid % 2 == 0 then return loop { up, dn } 
    else                 return loop { dn, up } end
 end
 
 -- update the tremolo parameters and trigger the envelope
-local time_last = { 0, 0 } -- last time each output's envelope was triggered
-function change(sid)
+local time_last = 0 -- last time the envelope was triggered
+function change()
    local time_now = time()
-   local time_delta = (time_now - time_last[sid]) * M_PERIOD -- seconds (1/f)
-   time_last[sid] = time_now -- record the current time
+   local time_delta = (time_now - time_last) * M_PERIOD -- seconds (1/f)
+   time_last = time_now -- record the current time
    
    -- capture pitch, create and trigger the tremolo envelope for specified out
    local pitch = input[PITCH].volts -- unused; could affect tremolo depth
